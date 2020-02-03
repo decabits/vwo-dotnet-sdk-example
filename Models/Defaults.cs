2c3a54cbeb15cdf5d0508666816ede5b8695ee99
Sahil Batla <sahilbathla1@gmail.com>
2020-02-03 15:56:05 +0530

    Remove demo app and use actual credentials

--- Models/Defaults.cs
+++ Models/Defaults.cs
@@ -4,10 +4,10 @@ namespace VWOSdk.DemoApp
 {
     internal class Defaults
     {
-        public readonly static long AccountId = 123456;
-        public readonly static string SdkKey = "sampleSdkKey";
-        public readonly static string CampaignTestKey = "DEV_TEST_6";          ////Assign actual value;
-        public readonly static string GoalIdentifier = "CUSTOM";          ////Assign actual value;
+        public readonly static long AccountId = 89499;
+        public readonly static string SdkKey = "7aeed7f67f5a0b0fbe476c1f086a7038";
+        public readonly static string CampaignTestKey = "phpab3";          ////Assign actual value;
+        public readonly static string GoalIdentifier = "custom";          ////Assign actual value;
         public readonly static Dictionary<string, dynamic> RevenueVariables = new Dictionary<string, dynamic>
         {
             {"revenue_value", 10}
