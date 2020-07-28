1bf52afc02a9345b1da60d9404f4e83cbc52247f
Sahil Batla <sahilbathla1@gmail.com>
2020-07-28 13:43:38 +0530

    remove redundant users

--- VWOSdk.DemoApp/Controllers/HomeController.cs
+++ VWOSdk.DemoApp/Controllers/HomeController.cs
@@ -20,9 +20,7 @@ namespace VWOSdk.DemoApp.Controllers
                "Ashley", "Bill", "Chris", "Dominic", "Emma", "Faizan",
                 "Gimmy", "Harry", "Ian", "John", "King", "Lisa", "Mona", "Nina",
                 "Olivia", "Pete", "Queen", "Robert", "Sarah", "Tierra", "Una",
-                "Varun", "Will", "Xin", "You", "Zeba",
-                "Varun",  "You", "Faizan", "Ashley", "Bill", "Chris", "Dominic",
-                "Rohit" , "Bob"
+                "Will", "Xin", "You", "Zeba",
                };
 
         private static Settings SettingsFile { get; set; }
